02c624cfc2dc9d8a9bf87b48f1cdc0522c9f12df
BaiJiazm <1481977509@qq.com>
2018-08-22 23:30:24 +0800

    添加 单调栈 题目

---
+++ Leetcode/Other/84. Largest Rectangle in Histogram.cpp
@@ -0,0 +1,20 @@
+//单调栈经典题目
+//详见https://zhuanlan.zhihu.com/p/26465701 
+class Solution {
+	public:
+		int largestRectangleArea(vector<int> &height) {
+			int ret = 0;
+			height.push_back(0);
+			vector<int> index;
+			for(int i = 0; i < height.size(); i++) {
+				while(index.size() > 0 && height[index.back()] >= height[i]) {
+					int h = height[index.back()];
+					index.pop_back();
+					int sidx = index.size() > 0 ? index.back() : -1;
+					ret = max(ret, h * (i-sidx-1));
+				}
+				index.push_back(i);
+			}
+			return ret;
+		}
+};
